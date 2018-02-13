c5994dcb05b17e122202314fd9647d7c81b6b859
ostoich <ostoich@users.noreply.github.com>
2018-02-13 21:42:05 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 			targets: ["SwiftAWSS3"]),
 		],
 	dependencies:[
-		.package(url:"https://github.com/CryptonautAI/SwiftAWSSignatureV4.git", from:"1.0.1")
+		.package(url:"https://github.com/CryptonautAI/SwiftAWSSignatureV4.git", from:"1.0.2")
 		,.package(url:"https://github.com/benspratling4/SwiftPatterns.git", from:"2.0.0")
 	],
 	targets:[
