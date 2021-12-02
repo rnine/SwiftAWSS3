dc6fc33dbd0a1e16677ecc5ba7829da049b507e0
Ruben Nine <ruben@9labs.io>
2021-12-02 13:52:08 +0100

    Using forked SwiftAWSSignatureV4 implementation.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 			targets: ["SwiftAWSS3"]),
 		],
 	dependencies:[
-		.package(url:"https://github.com/CryptonautAI/SwiftAWSSignatureV4.git", from:"1.0.3")
+		.package(url:"https://github.com/rnine/SwiftAWSSignatureV4.git", from:"1.1.0")
 	],
 	targets:[
 		.target(
